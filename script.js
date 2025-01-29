30399f4cdde4e9b6dd8ff69fd3e59818ed46763e
Jonas Desodt <jonasdesodt@gmail.com>
2025-01-29 10:51:07 +0100

    basic spa

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-document.getElementById('up').addEventListener('click', function(){
-    window.scrollTo(0,0);
+document.getElementById('menu-button').addEventListener('click', function(){
+    document.body.classList.toggle('menu-open');
+});
+
+let activeArticle = document.querySelector('main > article.active');
+
+let menu = document.getElementById('menu_items');
+let activeMenuItem = menu.querySelector('li.active');
+let menuItems = menu.querySelectorAll('li');
+
+
+Array.from(menuItems).forEach(function(menuItem){
+    menuItem.querySelector('button').addEventListener('click', function(){   
+        activeArticle.classList.remove('active'); 
+        activeMenuItem.classList.remove('active');       
+
+        let article = document.getElementById(menuItem.textContent.toLowerCase());
+        
+        article.classList.add('active');
+        menuItem.classList.add('active');
+
+        activeArticle = article;
+        activeMenuItem = menuItem;
+
+        document.body.classList.remove('menu-open');
+    });
 });
